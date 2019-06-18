74eede5c65655f1a9cbd7a678cf35c3910953f1f
Yi Ju Chung <b04704086@ntu.edu.tw>
2019-06-18 18:32:55 +0800

    final

---
+++ Final/final.js
@@ -0,0 +1,306 @@
+$(function() {
+    $('#btn1').click(function() {
+        var num = Math.floor(Math.random() * 32);
+        $('#imagech').attr('src', 'img/' + num + '.jpg')
+    })
+})
+
+
+var photo_wrap = document.getElementById('photo1');
+photo_wrap.onclick = function() {
+    var str = this.className;
+    if (/photo-front/.test(str)) {
+        this.className = str.replace(/photo-front/, 'photo-back');
+    } else if (/photo-back/.test(str)) {
+        this.className = str.replace(/photo-back/, 'photo-front');
+    }
+}
+
+var photo_wrap = document.getElementById('photo2');
+photo_wrap.onclick = function() {
+    var str = this.className;
+    if (/photo-front/.test(str)) {
+        this.className = str.replace(/photo-front/, 'photo-back');
+    } else if (/photo-back/.test(str)) {
+        this.className = str.replace(/photo-back/, 'photo-front');
+    }
+}
+
+var photo_wrap = document.getElementById('photo3');
+photo_wrap.onclick = function() {
+    var str = this.className;
+    if (/photo-front/.test(str)) {
+        this.className = str.replace(/photo-front/, 'photo-back');
+    } else if (/photo-back/.test(str)) {
+        this.className = str.replace(/photo-back/, 'photo-front');
+    }
+}
+
+var photo_wrap = document.getElementById('photo4');
+photo_wrap.onclick = function() {
+    var str = this.className;
+    if (/photo-front/.test(str)) {
+        this.className = str.replace(/photo-front/, 'photo-back');
+    } else if (/photo-back/.test(str)) {
+        this.className = str.replace(/photo-back/, 'photo-front');
+    }
+}
+
+var photo_wrap = document.getElementById('photo5');
+photo_wrap.onclick = function() {
+    var str = this.className;
+    if (/photo-front/.test(str)) {
+        this.className = str.replace(/photo-front/, 'photo-back');
+    } else if (/photo-back/.test(str)) {
+        this.className = str.replace(/photo-back/, 'photo-front');
+    }
+}
+
+var photo_wrap = document.getElementById('photo6');
+photo_wrap.onclick = function() {
+    var str = this.className;
+    if (/photo-front/.test(str)) {
+        this.className = str.replace(/photo-front/, 'photo-back');
+    } else if (/photo-back/.test(str)) {
+        this.className = str.replace(/photo-back/, 'photo-front');
+    }
+}
+
+var photo_wrap = document.getElementById('photo7');
+photo_wrap.onclick = function() {
+    var str = this.className;
+    if (/photo-front/.test(str)) {
+        this.className = str.replace(/photo-front/, 'photo-back');
+    } else if (/photo-back/.test(str)) {
+        this.className = str.replace(/photo-back/, 'photo-front');
+    }
+}
+var photo_wrap = document.getElementById('photo8');
+photo_wrap.onclick = function() {
+    var str = this.className;
+    if (/photo-front/.test(str)) {
+        this.className = str.replace(/photo-front/, 'photo-back');
+    } else if (/photo-back/.test(str)) {
+        this.className = str.replace(/photo-back/, 'photo-front');
+    }
+}
+var photo_wrap = document.getElementById('photo9');
+photo_wrap.onclick = function() {
+    var str = this.className;
+    if (/photo-front/.test(str)) {
+        this.className = str.replace(/photo-front/, 'photo-back');
+    } else if (/photo-back/.test(str)) {
+        this.className = str.replace(/photo-back/, 'photo-front');
+    }
+}
+var photo_wrap = document.getElementById('photo10');
+photo_wrap.onclick = function() {
+    var str = this.className;
+    if (/photo-front/.test(str)) {
+        this.className = str.replace(/photo-front/, 'photo-back');
+    } else if (/photo-back/.test(str)) {
+        this.className = str.replace(/photo-back/, 'photo-front');
+    }
+}
+var photo_wrap = document.getElementById('photo11');
+photo_wrap.onclick = function() {
+    var str = this.className;
+    if (/photo-front/.test(str)) {
+        this.className = str.replace(/photo-front/, 'photo-back');
+    } else if (/photo-back/.test(str)) {
+        this.className = str.replace(/photo-back/, 'photo-front');
+    }
+}
+var photo_wrap = document.getElementById('photo12');
+photo_wrap.onclick = function() {
+    var str = this.className;
+    if (/photo-front/.test(str)) {
+        this.className = str.replace(/photo-front/, 'photo-back');
+    } else if (/photo-back/.test(str)) {
+        this.className = str.replace(/photo-back/, 'photo-front');
+    }
+}
+var photo_wrap = document.getElementById('photo13');
+photo_wrap.onclick = function() {
+    var str = this.className;
+    if (/photo-front/.test(str)) {
+        this.className = str.replace(/photo-front/, 'photo-back');
+    } else if (/photo-back/.test(str)) {
+        this.className = str.replace(/photo-back/, 'photo-front');
+    }
+}
+var photo_wrap = document.getElementById('photo14');
+photo_wrap.onclick = function() {
+    var str = this.className;
+    if (/photo-front/.test(str)) {
+        this.className = str.replace(/photo-front/, 'photo-back');
+    } else if (/photo-back/.test(str)) {
+        this.className = str.replace(/photo-back/, 'photo-front');
+    }
+}
+var photo_wrap = document.getElementById('photo15');
+photo_wrap.onclick = function() {
+    var str = this.className;
+    if (/photo-front/.test(str)) {
+        this.className = str.replace(/photo-front/, 'photo-back');
+    } else if (/photo-back/.test(str)) {
+        this.className = str.replace(/photo-back/, 'photo-front');
+    }
+}
+var photo_wrap = document.getElementById('photo16');
+photo_wrap.onclick = function() {
+    var str = this.className;
+    if (/photo-front/.test(str)) {
+        this.className = str.replace(/photo-front/, 'photo-back');
+    } else if (/photo-back/.test(str)) {
+        this.className = str.replace(/photo-back/, 'photo-front');
+    }
+}
+var photo_wrap = document.getElementById('photo17');
+photo_wrap.onclick = function() {
+    var str = this.className;
+    if (/photo-front/.test(str)) {
+        this.className = str.replace(/photo-front/, 'photo-back');
+    } else if (/photo-back/.test(str)) {
+        this.className = str.replace(/photo-back/, 'photo-front');
+    }
+}
+var photo_wrap = document.getElementById('photo18');
+photo_wrap.onclick = function() {
+    var str = this.className;
+    if (/photo-front/.test(str)) {
+        this.className = str.replace(/photo-front/, 'photo-back');
+    } else if (/photo-back/.test(str)) {
+        this.className = str.replace(/photo-back/, 'photo-front');
+    }
+}
+var photo_wrap = document.getElementById('photo19');
+photo_wrap.onclick = function() {
+    var str = this.className;
+    if (/photo-front/.test(str)) {
+        this.className = str.replace(/photo-front/, 'photo-back');
+    } else if (/photo-back/.test(str)) {
+        this.className = str.replace(/photo-back/, 'photo-front');
+    }
+}
+var photo_wrap = document.getElementById('photo20');
+photo_wrap.onclick = function() {
+    var str = this.className;
+    if (/photo-front/.test(str)) {
+        this.className = str.replace(/photo-front/, 'photo-back');
+    } else if (/photo-back/.test(str)) {
+        this.className = str.replace(/photo-back/, 'photo-front');
+    }
+}
+var photo_wrap = document.getElementById('photo21');
+photo_wrap.onclick = function() {
+    var str = this.className;
+    if (/photo-front/.test(str)) {
+        this.className = str.replace(/photo-front/, 'photo-back');
+    } else if (/photo-back/.test(str)) {
+        this.className = str.replace(/photo-back/, 'photo-front');
+    }
+}
+
+var photo_wrap = document.getElementById('photo22');
+photo_wrap.onclick = function() {
+    var str = this.className;
+    if (/photo-front/.test(str)) {
+        this.className = str.replace(/photo-front/, 'photo-back');
+    } else if (/photo-back/.test(str)) {
+        this.className = str.replace(/photo-back/, 'photo-front');
+    }
+}
+
+var photo_wrap = document.getElementById('photo23');
+photo_wrap.onclick = function() {
+    var str = this.className;
+    if (/photo-front/.test(str)) {
+        this.className = str.replace(/photo-front/, 'photo-back');
+    } else if (/photo-back/.test(str)) {
+        this.className = str.replace(/photo-back/, 'photo-front');
+    }
+}
+var photo_wrap = document.getElementById('photo24');
+photo_wrap.onclick = function() {
+    var str = this.className;
+    if (/photo-front/.test(str)) {
+        this.className = str.replace(/photo-front/, 'photo-back');
+    } else if (/photo-back/.test(str)) {
+        this.className = str.replace(/photo-back/, 'photo-front');
+    }
+}
+var photo_wrap = document.getElementById('photo25');
+photo_wrap.onclick = function() {
+    var str = this.className;
+    if (/photo-front/.test(str)) {
+        this.className = str.replace(/photo-front/, 'photo-back');
+    } else if (/photo-back/.test(str)) {
+        this.className = str.replace(/photo-back/, 'photo-front');
+    }
+}
+var photo_wrap = document.getElementById('photo26');
+photo_wrap.onclick = function() {
+    var str = this.className;
+    if (/photo-front/.test(str)) {
+        this.className = str.replace(/photo-front/, 'photo-back');
+    } else if (/photo-back/.test(str)) {
+        this.className = str.replace(/photo-back/, 'photo-front');
+    }
+}
+var photo_wrap = document.getElementById('photo27');
+photo_wrap.onclick = function() {
+    var str = this.className;
+    if (/photo-front/.test(str)) {
+        this.className = str.replace(/photo-front/, 'photo-back');
+    } else if (/photo-back/.test(str)) {
+        this.className = str.replace(/photo-back/, 'photo-front');
+    }
+}
+var photo_wrap = document.getElementById('photo28');
+photo_wrap.onclick = function() {
+    var str = this.className;
+    if (/photo-front/.test(str)) {
+        this.className = str.replace(/photo-front/, 'photo-back');
+    } else if (/photo-back/.test(str)) {
+        this.className = str.replace(/photo-back/, 'photo-front');
+    }
+}
+var photo_wrap = document.getElementById('photo29');
+photo_wrap.onclick = function() {
+    var str = this.className;
+    if (/photo-front/.test(str)) {
+        this.className = str.replace(/photo-front/, 'photo-back');
+    } else if (/photo-back/.test(str)) {
+        this.className = str.replace(/photo-back/, 'photo-front');
+    }
+}
+var photo_wrap = document.getElementById('photo30');
+photo_wrap.onclick = function() {
+    var str = this.className;
+    if (/photo-front/.test(str)) {
+        this.className = str.replace(/photo-front/, 'photo-back');
+    } else if (/photo-back/.test(str)) {
+        this.className = str.replace(/photo-back/, 'photo-front');
+    }
+}
+
+var photo_wrap = document.getElementById('photo31');
+photo_wrap.onclick = function() {
+    var str = this.className;
+    if (/photo-front/.test(str)) {
+        this.className = str.replace(/photo-front/, 'photo-back');
+    } else if (/photo-back/.test(str)) {
+        this.className = str.replace(/photo-back/, 'photo-front');
+    }
+}
+
+var photo_wrap = document.getElementById('photo0');
+photo_wrap.onclick = function() {
+    var str = this.className;
+    if (/photo-front/.test(str)) {
+        this.className = str.replace(/photo-front/, 'photo-back');
+    } else if (/photo-back/.test(str)) {
+        this.className = str.replace(/photo-back/, 'photo-front');
+    }
+}
